cf21808517e41f5e5f1309366ace15e582db3cf0
Samuel Craig Carey <sccarey@sandia.gov>
2022-12-13 17:52:54 -0700

    add web server URL

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -8,4 +8,5 @@ pub mod new_poll;
 pub mod participation;
 pub mod retrieve;
 
-pub const SERVER_URL: &str = "http://127.0.0.1:3000";
+// pub const SERVER_URL: &str = "http://127.0.0.1:3000";
+pub const SERVER_URL: &str = "http://ec2-34-216-22-62.us-west-2.compute.amazonaws.com:3000";
