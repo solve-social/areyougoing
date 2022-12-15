328b8bc51cce4ef32f294ade2e4e37fc846b77fb
Samuel Craig Carey <sccarey@sandia.gov>
2022-12-14 21:22:06 -0700

    switch server back to public IP

--- server/src/main.rs
+++ server/src/main.rs
@@ -66,8 +66,8 @@ async fn main() {
         .layer(Extension(config))
         .layer(Extension(Arc::new(Mutex::new(db))));
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000)); // for offline use
-                                                         // let addr = SocketAddr::from((local_ip().expect("Failed to get local ip address"), 3000));
+    // let addr = SocketAddr::from(([127, 0, 0, 1], 3000)); // for offline use
+    let addr = SocketAddr::from((local_ip().expect("Failed to get local ip address"), 3000));
     println!("Listening on http://{addr}");
     axum::Server::bind(&addr)
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
